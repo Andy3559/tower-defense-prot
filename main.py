4c92936ac99c3e4ecad890f5a8f4cbba04e3e04f
Andy Chen <andyandy.uk.uk@gmail.com>
2023-09-30 16:49:28 +0100

    Andys first commit

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-Python 3.8.10 (default, Mar 13 2023, 10:26:41) 
-[GCC 9.4.0] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print("hello world")
+
+import pyglet
+from pyglet.window import key
+score = 23  
+
+new_window = pyglet.window.Window()
+keys = key.KeyStateHandler()
+new_window.push_handlers(keys)
+  
+label = pyglet.text.Label('Hello, World its me!',
+                          font_name ='Cooper',
+                          font_size = 16,
+                          x = new_window.width//2, 
+                          y = new_window.height//2,
+                          anchor_x ='center', 
+                          anchor_y ='center')
+
+
+@new_window.event
+def on_key_press(symbol, modifiers):
+    global score
+    score= score+1
+    if score <=> 100:
+        score= score+100
+    print(score)
+    
+  
+@new_window.event
+def on_draw():
+    score = 0
+    new_window.clear()
+    label.draw()
+
+  
+pyglet.app.run()

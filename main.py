71792e10f05966c78cdefff1f48d2bc4bdb28e51
Andy Chen <andyandy.uk.uk@gmail.com>
2023-10-14 16:11:12 +0100

    working version

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
 import pyglet
 from pyglet.window import key
-score = 23  
+score = 950 
 
 new_window = pyglet.window.Window()
 keys = key.KeyStateHandler()
@@ -15,14 +14,18 @@ label = pyglet.text.Label('Hello, World its me!',
                           anchor_x ='center', 
                           anchor_y ='center')
 
+score_label = pyglet.text.Label(text="Score: 0", x=10, y=460)
 
 @new_window.event
 def on_key_press(symbol, modifiers):
     global score
     score= score+1
-    if score <=> 100:
-        score= score+100
+    if score > 1000 :
+        score= score+999
     print(score)
+    score_str = str(score)
+    score_label.text = score_str
+    #score_label.update()
     
   
 @new_window.event
@@ -30,6 +33,8 @@ def on_draw():
     score = 0
     new_window.clear()
     label.draw()
+    score_label.draw()
+
 
   
 pyglet.app.run()
